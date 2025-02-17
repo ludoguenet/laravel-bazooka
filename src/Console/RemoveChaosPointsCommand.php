78d1047f91ec4f00e820f091ee00abdeb4647aef
ludoguenet <ludovicguenet@gmx.com>
2025-02-17 17:31:42 +0100

    Refactor RemoveChaosPointsCommand to update regex pattern for removing Bazooka::chaos() while preserving indentation and surrounding code

--- src/Console/RemoveChaosPointsCommand.php
+++ src/Console/RemoveChaosPointsCommand.php
@@ -27,7 +27,7 @@ class RemoveChaosPointsCommand extends Command
             $content = $file->getContents();
             // Pattern pour capturer l'indentation, la ligne Bazooka::chaos(), et le reste du code
             $pattern = '/(\s*)\{[\s\n]*\\\\LaravelJutsu\\\\Bazooka\\\\Facades\\\\Bazooka::chaos\(\);[\s\n]*([^}]+)\}/s';
-            // Remplacement pour conserver l'indentation et le reste du code
+            // Remplacement pour supprimer uniquement la ligne Bazooka::chaos() et conserver le reste
             $replacement = '$1{'.PHP_EOL.'$1    $2'.PHP_EOL.'$1}';
             $newContent = preg_replace($pattern, $replacement, $content, -1, $count);
 
