e71e0f05f1bc5b0c7524ba0b0c3c79b8d700e6bb
ludoguenet <ludovicguenet@gmx.com>
2025-02-17 17:46:38 +0100

    Refactor BazookaServiceProvider to improve configuration merging and strategy addition logic

--- src/BazookaServiceProvider.php
+++ src/BazookaServiceProvider.php
@@ -15,6 +15,7 @@ class BazookaServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        // Fusionner la configuration avec celle existante
         $this->mergeConfigFrom(
             __DIR__.'/config/bazooka.php', 'bazooka'
         );
@@ -23,6 +24,7 @@ class BazookaServiceProvider extends ServiceProvider
             $config = $app['config']['bazooka'];
             $manager = new BazookaManager($config);
 
+            // Ajouter des stratégies en fonction de la configuration
             if ($config['strategies']['latency']['enabled']) {
                 $manager->addStrategy(new LatencyDisruption($config['strategies']['latency']));
             }
@@ -39,19 +41,19 @@ class BazookaServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/config/bazooka.php' => config_path('bazooka.php'),
-        ]);
+        ], 'config');
 
         if ($this->app->runningInConsole()) {
-            $this->optimizes(
-                optimize: 'package:optimize',
-                clear: 'package:clear-optimizations',
-            );
-
             $this->commands([
                 InjectChaosCommand::class,
                 ListChaosPointsCommand::class,
                 RemoveChaosPointsCommand::class,
             ]);
+
+            $this->optimizes(
+                optimize: 'package:optimize',
+                clear: 'package:clear-optimizations',
+            );
         }
     }
 }
